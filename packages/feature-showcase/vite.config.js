c7f882fd53fd0fd0a2293ab4a0ef68a4327f9371
ginguyen <ginguyen@axon.com>
2025-12-24 19:25:44 +0700

    refactor: update Vite configuration to define development mode for improved build clarity

--- packages/feature-showcase/vite.config.js
+++ packages/feature-showcase/vite.config.js
@@ -24,6 +24,9 @@ function watchStorion() {
 export default defineConfig({
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     plugins: [react(), watchStorion()],
+    define: {
+        __DEV__: JSON.stringify(true),
+    },
     resolve: {
         alias: [
             // More specific paths first
